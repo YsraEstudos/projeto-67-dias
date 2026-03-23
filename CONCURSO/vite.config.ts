275715b2a3eaa7b565e28e599c806f64af2262b2
Ysrael machado <86720970097@ulife.com.br>
2026-03-23 08:42:38 -0300

    Make concurso reuse root env config

--- CONCURSO/vite.config.ts
+++ CONCURSO/vite.config.ts
@@ -1,9 +1,11 @@
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/concurso/',
+  envDir: resolve(__dirname, '..'),
   plugins: [
     react(),
   ],
